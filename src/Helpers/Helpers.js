dc9307ac6b4f41ca2f55f84297cc34eff94b27b2
rei-rala <ramonirala@outlook.com>
2021-08-31 01:25:28 -0300

    Modified Helpers.js - standalone: up to date

--- src/Helpers/Helpers.js
+++ src/Helpers/Helpers.js
@@ -1,3 +1,12 @@
+export const digestMessage = async (message) => {
+  const msgUint8 = new TextEncoder().encode(message);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+  return hashHex;
+}
+
 // Esta funcion nos permitira acceder a nesting en arrays y otras validaciones =P
 export const tryParseInt = (numberLike) => {
   return (
