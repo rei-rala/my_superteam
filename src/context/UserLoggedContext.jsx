6467414a2a5c77b6bba30a7baffc3fcfe3c5af1e
rei-rala <ramonirala@outlook.com>
2021-09-01 15:12:42 -0300

    up to date

--- src/context/UserLoggedContext.jsx
+++ src/context/UserLoggedContext.jsx
@@ -17,15 +17,31 @@ export const UserLoggedContext = ({ children }) => {
 
   const [areCredsValid, setAreCredsValid] = useState(false)
 
+  const authUser = (encryptedMail, encryptedPassword) => {
+    const users = [
+      {
+        mail: '9bc5e0d6ba78769e7aea29c57d9315348a6d879ac22a17b68cd14fe00e1f1478',
+        password: '275976081ce1abf67779eb3c388b5e14531082e52137502e264776e1a6a11595',
+      },
+      {
+        mail: '97d0f744548aa446bba6cbb198d375bd8b224429f59093016b7fa90d1b3febda',
+        password: '76cb73cd1829daa4fa75788e27a756600b5f62ab03f2a9bd15d4a56644a22b78',
+      },
+      {
+        mail: '957b96fcb88c20e3d8d3abe3ab0685cbbac8734b7dc0f928dbb6a8794dd74e9f',
+        password: 'f680e0021dcaf15d161604378236937225eeecae85cc6cda09ea85fad4cc51bb'
+      }
+    ]
+    const u = users.find(u => u.mail === encryptedMail)
+    const isOk = u?.password === encryptedPassword || false
 
-  const axios = require('axios').default
-  const CancelToken = axios.CancelToken;
-  const source = CancelToken.source()
+    return isOk
+  }
+
 
   const tryLogIn = async (ev) => {
     ev.preventDefault();
     ev.stopPropagation();
-
     const lowerMail = (ev.target.userEmail.value).toLowerCase()
 
     logIn({
@@ -34,52 +50,47 @@ export const UserLoggedContext = ({ children }) => {
     })
   }
 
+
   const logIn = async (userInfoInput) => {
     setLoggingIn(true)
-    const mailEnc = await digestMessage(userInfoInput.email)
+    const digestMail = await digestMessage(userInfoInput.email);
+    const digestPass = await digestMessage(userInfoInput.password);
+    const validCredential = authUser(digestMail, digestPass)
+    const shortUsername = (userInfoInput.email).split('@')[0]
 
-    axios({
-      method: 'post',
-      url: 'http://challenge-react.alkemy.org/',
-      data: userInfoInput
-    }, {
-      cancelToken: source.token,
+    new Promise((resolve, reject) => {
+      validCredential && resolve({
+        ok: true,
+        token: 'STANDALONE',
+        shortEmail: shortUsername,
+        email: userInfoInput.email,
+        authMail: digestMail
+      })
+
+      !validCredential && reject({ ok: false })
     })
       .then(r => {
-        try {
+        if (r.ok) {
+          localStorage.setItem('superteam_tokenStandalone', r.token)
+          localStorage.setItem('superteam_email', r.email)
+          setUsernameLogged(r.email)
+          setShortUsernameLogged(r.shortEmail)
+          setUserInfo({ ...userInfoInput, emailEncrypted: r.authMail })
 
-          if (r.status === 200) {
-            console.log(`Iniciado con exito. Codigo ${r.status}`)
-
-            localStorage.setItem('superteam_token', r.data.token)
-            localStorage.setItem('superteam_email', userInfoInput.email)
-            setUsernameLogged(userInfoInput.email)
-
-            const shortUsername = (userInfoInput.email).split('@')[0]
-            setShortUsernameLogged(
-              (
-                shortUsername.length > 12
-                  ? shortUsername.slice(0, 9) + '...'
-                  : shortUsername
-              ).toUpperCase()
-            )
-            setUserInfo({ ...userInfoInput, emailEncrypted: mailEnc })
-            setAreCredsValid(true)
-
-          }
-          else throw new Error('Error')
+          setAreCredsValid(true)
         }
-        catch (err) {
-          setTriedWrongCredentials({ error: true })
+        else {
+          throw new Error(r.ok)
         }
       })
       .catch(err => {
-        setTriedWrongCredentials({ error: true, type: err })
+        setTriedWrongCredentials({ error: !err.ok, type: 'Credenciales incorrectas' })
         setAreCredsValid(false)
       })
       .finally(() => setLoggingIn(false))
-
   }
+
+
 
   useEffect(() => {
     areCredsValid && setUserLogged(true)
@@ -103,22 +114,19 @@ export const UserLoggedContext = ({ children }) => {
   useEffect(() => {
     const tryRetrieveLocalSession = async () => {
       const sessionLS = {
-        token: localStorage.getItem('superteam_token'),
+        token: localStorage.getItem('superteam_tokenStandalone'),
         email: localStorage.getItem('superteam_email')
       }
 
-      if (
-        sessionLS.email === 'challenge@alkemy.org'
-        && sessionLS.token === 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiZW1haWwiOiJjaGFsbGVuZ2VAYWxrZW15Lm9yZyIsImlhdCI6MTUxNjIzOTAyMn0.ilhFPrG0y7olRHifbjvcMOlH7q2YwlegT0f4aSbryBE'
-      ) {
+      if ((sessionLS.email === 'challenge@alkemy.org' || sessionLS.email === 'ramonirala@superteam.com' || sessionLS.email === 'rimi@superteam.com')
+        && sessionLS.token === 'STANDALONE') {
+
         const shortedLocalStorage = ((sessionLS.email).split('@')[0]).toUpperCase()
+        const mailenc = await digestMessage(sessionLS.email)
 
         setUsernameLogged(sessionLS.email)
         setShortUsernameLogged(shortedLocalStorage)
 
-
-        // hardcoded por ahora
-        const mailenc = await digestMessage('challenge@alkemy.org')
         setUserInfo({ email: sessionLS.email, emailEncrypted: mailenc })
         setAreCredsValid(true)
       }
