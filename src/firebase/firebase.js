edbd8245deaa3128d57e228975b0022b7397f575
rei-rala <ramonirala@outlook.com>
2021-09-23 23:19:23 -0300

    env variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,21 +2,15 @@ import firebase from '@firebase/app-compat';
 import 'firebase/compat/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBoJR_o-5K57sz1S1BXjkv-es4vA-Dl6sQ",
-
-  authDomain: "stdb-d6810.firebaseapp.com",
-
-  projectId: "stdb-d6810",
-
-  storageBucket: "stdb-d6810.appspot.com",
-
-  messagingSenderId: "746346632230",
-
-  appId: "1:746346632230:web:83693eade38b6926c982e7"
-
+  appId: process.env.REACT_APP_appId,
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
 };
 
-
+console.table(firebaseConfig)
 const app = firebase.initializeApp(firebaseConfig);
 export function getFirebase() {
   return app;
